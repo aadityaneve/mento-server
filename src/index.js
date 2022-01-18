f979753240642cf660a35592a953acf90c9f6a8c
Aditya Neve <aditya.neve@gmail.com>
2022-01-18 15:37:54 +0530

    home route added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ app.use(express.json());
 const topicsController = require('./controllers/topics.controller');
 const topicsDetailsController = require('./controllers/topicsDetails.controller');
 
+app.use('/', (req, res) => {
+    try {
+        const home = 'Welcome';
+
+        return res.status(201).send(home);
+    } catch (e) {
+        return res.status(500).send({ status: 'FAILED', message: e.message });
+    }
+});
 app.use('/', topicsController);
 app.use('/', topicsDetailsController);
 
